f334c93f2a2dcc33082e231b6e6449c8da57a1d0
이창준 <2018110021@dongguk.edu>
2024-11-29 04:28:23 +0900

    feat: 글로벌 스타일을 적용한다.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import {ThemeProvider} from "styled-components";
+import theme from "@/shared/theme.js";
+import {GlobalStyle} from "@/shared/global.js";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+          <GlobalStyle/>
+          <App/>
+      </ThemeProvider>
   </StrictMode>,
 )
